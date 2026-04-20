6b059d4045a300a35ba2d527a9ddbf4c4f8b0c3d
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-04-20 15:51:12 +0300

    1. Generate Results for subtraction, AND and OR

--- alusim.c
+++ alusim.c
@@ -14,6 +14,13 @@ unsigned int ALU(unsigned int A, unsigned int B, unsigned int opcode) {
   case 3: // OR
     result = A | B;
     break;
+  case 4: // XOR
+    result = A ^ B;
+    break;
+    // Implementing NOT and XOR
+  case 5: // NOT (operates on A only)
+    result = -A;
+    break;
   default:
     result = 0;
     break;
@@ -26,5 +33,13 @@ int main() {
   unsigned int opcode = 0; // 0 for addition
   unsigned int result = ALU(A, B, opcode);
   printf("Result of operation: %u\n", result);
+
+  // Results for subtraction, AND and OR
+  char *ops[] = {"ADD", "SUB", "AND", "OR", "XOR", "NOT"};
+
+  for (unsigned int opcode = 0; opcode <= 5; opcode++) {
+    unsigned int result = ALU(A, B, opcode);
+    printf("%s: %u\n", ops[opcode], result);
+  }
   return 0;
 }
